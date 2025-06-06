0a6df5ffbff4f4aab7a2fb007e846a9787bb09b8
Sophie Wirth <sophiewirth@MacBook-Pro.local>
2025-06-06 14:34:12 +0200

    img alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 import { DisplayImages } from "./DisplayImages";
-import img from "../../public/portfolioImg/displayImg/AmberSunset.jpeg"
+import img from "../../public/portfolioImg/displayImg/AmberSunset.jpeg";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <header className={styles.header}>Sophie Wirth - Artist Portfolio</header>
       <main className={styles.main}>
-        {
-          Object.keys(DisplayImages).map((key) => {
-            console.log(key, DisplayImages[key])
+        <div className={styles.gallery}>
+          {Object.keys(DisplayImages).map((key) => {
+            console.log(key, DisplayImages[key]);
             return (
-              <Image src={DisplayImages[key].src} alt={DisplayImages[key].alt} width={DisplayImages[key].width/20} height={DisplayImages[key].height/20} key={key}/>
-            )
-          })
-        }
+              <Image
+                src={DisplayImages[key].src}
+                alt={DisplayImages[key].alt}
+                width={DisplayImages[key].width / (DisplayImages[key].height/300)}
+                height={300}
+                key={key}
+              />
+            );
+          })}
+        </div>
       </main>
-      <footer className={styles.footer}>
-        footer
-      </footer>
+      <footer className={styles.footer}>footer</footer>
     </div>
   );
 }
