a912adb60b1334dc62a07d81d7b360f025adcbd3
Sophie Wirth <sophiewirth@macbook-pro.speedport.ip>
2025-06-06 11:11:02 +0200

    display imgs

--- src/app/DisplayImages.tsx
+++ src/app/DisplayImages.tsx
@@ -1,7 +1,29 @@
 import Image from "next/image";
-import img from "../../public/portfolioImg/displayImg/AmberSunset.jpeg"
+import AmberSunset from "../../public/portfolioImg/displayImg/AmberSunset.jpeg"
+import DragonHeart from "../../public/portfolioImg/displayImg/DragonHeart.jpeg"
+import HopefulSunset from "../../public/portfolioImg/displayImg/HopefulSunset.jpeg"
+import RainbowSunset from "../../public/portfolioImg/displayImg/RainbowSunset.jpeg"
+import ChaosStorm from "../../public/portfolioImg/displayImg/ChaosStorm.jpeg"
+import DreamySunset from "../../public/portfolioImg/displayImg/DreamySunset.jpeg"
+import Nebula from "../../public/portfolioImg/displayImg/Nebula.jpeg"
+import Stormy from "../../public/portfolioImg/displayImg/Stormy.jpeg"
+import ChildPortrait from "../../public/portfolioImg/displayImg/ChildPortrait.jpeg"
+import FierySunset from "../../public/portfolioImg/displayImg/FierySunset.jpeg"
+import Pastels from "../../public/portfolioImg/displayImg/Pastels.jpeg"
+import WaterDragon from "../../public/portfolioImg/displayImg/WaterDragon.jpeg"
 
 // export const diim =[{..img, alt="Amber Sunset"}]
-export const DisplayImages = [
-  {...img, alt:"Amber Sunset"},
-]
+export const DisplayImages = {
+  AmberSunset: {...AmberSunset, alt:"Amber Sunset"},
+  HopefulSunset: {...HopefulSunset, alt:"Hopeful Sunset"},
+  RainbowSunset: {...RainbowSunset, alt:"Rainbow Sunset"},
+  ChaosStorm: {...ChaosStorm, alt:"Chaos Storm"},
+  DreamySunset: {...DreamySunset, alt:"Dreamy Sunset"},
+  Nebula: {...Nebula, alt:"Nebula"},
+  Stormy: {...Stormy, alt:"Stormy"},
+  ChildPortrait: {...ChildPortrait, alt:"Child Portrait"},
+  FierySunset: {...FierySunset, alt:"Fiery Sunset"},
+  Pastels: {...Pastels, alt:"Pastels"},
+  WaterDragon: {...WaterDragon, alt:"Water Dragon"},
+  DragonHeart: {...DragonHeart, alt:"Dragon Heart"},
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-import { diim } from "./DisplayImages";
 import { DisplayImages } from "./DisplayImages";
 import img from "../../public/portfolioImg/displayImg/AmberSunset.jpeg"
 
@@ -10,14 +9,13 @@ export default function Home() {
       <header className={styles.header}>Sophie Wirth - Artist Portfolio</header>
       <main className={styles.main}>
         {
-          DisplayImages.map((img) => {
-            console.log('img[keys]', img)
+          Object.keys(DisplayImages).map((key) => {
+            console.log(key, DisplayImages[key])
             return (
-              <Image src={img} alt={img.alt} width={img.width/20} height={img.height/20} key={img.src}/>
+              <Image src={DisplayImages[key].src} alt={DisplayImages[key].alt} width={DisplayImages[key].width/20} height={DisplayImages[key].height/20} key={key}/>
             )
           })
         }
-        {/* <Image src={img} alt="Amber Sunset" width={img.width/20} height={img.height/20}/> */}
       </main>
       <footer className={styles.footer}>
         footer
