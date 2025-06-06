a3fca37c2bd70167cb6c67c0524b8d00d6da3a2c
Sophie Wirth <sophiewirth@macbook-pro.speedport.ip>
2025-06-06 10:40:21 +0200

    SIZES

---
+++ src/app/DisplayImages.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export const DisplayImages = [
+  <Image src="/portfolioImg/displayImg/AmberSunset.jpeg" alt="Amber Sunset" />,
+  <Image src="/portfolioImg/displayImg/DragonHeart.jpeg" alt="Dragon Heart" />,
+  <Image src="/portfolioImg/displayImg/HopefulSunset.jpeg" alt="Hopeful Sunset" />,
+  <Image src="/portfolioImg/displayImg/RainbowSunset.jpeg" alt="Rainbow Sunset" />,
+  <Image src="/portfolioImg/displayImg/ChaosStorm.jpeg" alt="Chaos Storm" />,
+  <Image src="/portfolioImg/displayImg/DreamySunset.jpeg" alt="Dreamy Sunset" />,
+  <Image src="/portfolioImg/displayImg/Nebula.jpeg" alt="Nebula" />,
+  <Image src="/portfolioImg/displayImg/Stormy.jpeg" alt="Stormy" />,
+  <Image src="/portfolioImg/displayImg/ChildPortrait.jpeg" alt="Child Portrait" />,
+  <Image src="/portfolioImg/displayImg/FierySunset.jpeg" alt="Fiery Sunset" />,
+  <Image src="/portfolioImg/displayImg/Pastels.jpeg" alt="Pastels" />,
+  <Image src="/portfolioImg/displayImg/WaterDragon.jpeg" alt="Water Dragon" />,
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
+import { DisplayImages } from "./DisplayImages";
+import img from "../../public/portfolioImg/displayImg/AmberSunset.jpeg"
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <header className={styles.header}>Sophie Wirth - Artist Portfolio</header>
       <main className={styles.main}>
-        asd
+        {/* {Object.entries(DisplayImages).map(([key, image]) => (
+          <Image
+            key={key}
+            src={image.props.src}
+            alt={image.props.alt}
+            width={image.props.width}
+            height={image.props.height}
+          />
+        ))} */}
+        <Image src={img} alt="Amber Sunset" width={img.width/20} height={img.height/20}/>
       </main>
       <footer className={styles.footer}>
         footer
