b8d10d9d3aa1274355fadc451ffc090380f28f1b
Sophie Wirth <sophiewirth@macbook-pro.speedport.ip>
2025-06-06 10:52:14 +0200

    alt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Image from "next/image";
+import { diim } from "./DisplayImages";
 import { DisplayImages } from "./DisplayImages";
 import img from "../../public/portfolioImg/displayImg/AmberSunset.jpeg"
 
@@ -8,16 +9,15 @@ export default function Home() {
     <div className={styles.page}>
       <header className={styles.header}>Sophie Wirth - Artist Portfolio</header>
       <main className={styles.main}>
-        {/* {Object.entries(DisplayImages).map(([key, image]) => (
-          <Image
-            key={key}
-            src={image.props.src}
-            alt={image.props.alt}
-            width={image.props.width}
-            height={image.props.height}
-          />
-        ))} */}
-        <Image src={img} alt="Amber Sunset" width={img.width/20} height={img.height/20}/>
+        {
+          DisplayImages.map((img) => {
+            console.log('img[keys]', img)
+            return (
+              <Image src={img} alt={img.alt} width={img.width/20} height={img.height/20} key={img.src}/>
+            )
+          })
+        }
+        {/* <Image src={img} alt="Amber Sunset" width={img.width/20} height={img.height/20}/> */}
       </main>
       <footer className={styles.footer}>
         footer
