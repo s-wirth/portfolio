ea98ca731b333f3dcd6444d68e6f7166b93e2084
Sophie Wirth <sophiewirth@macbook-pro.speedport.ip>
2025-06-08 10:33:29 +0200

    swap images onclick

--- src/app/Gallery/page.tsx
+++ src/app/Gallery/page.tsx
@@ -1,16 +1,43 @@
+'use client'
 import styles from "./page.module.css";
+import { useState } from 'react';
+
+
+const displayImages = [
+  {
+    src: "https://placehold.co/600x400",
+    alt: "Image 1",
+    thumbnail: "https://placehold.co/100x100",
+  },
+  {
+    src: "https://placehold.co/600x400/000000/FFF",
+    alt: "Image 2",
+    thumbnail: "https://placehold.co/100x100/000000/FFF",
+  },
+]
+
 export default function Gallery() {
+const [displayImageSrc, setDisplayImageSrc] = useState(displayImages[0].src); 
   return (
     <div className={styles.gallery_container}>
       <div className={styles.gallery_carousel}>
-        <img src="https://placehold.co/600x400" alt="Image 1" />
-        {/* <img src="https://placehold.co/600x400" alt="Image 2" />
-        <img src="https://placehold.co/600x400" alt="Image 3" /> */}
+        <div className={styles.gallery_carousel_btnright}></div>
+        <div className={styles.gallery_carousel_btnleft}></div>
+        <img className={styles.gallery_image} src={displayImageSrc} alt="Image 1" />
       </div>
       <div className={styles.gallery_thumbnails}>
-        <img src="https://placehold.co/100x100" alt="Thumbnail 1" />
-        <img src="https://placehold.co/100x100" alt="Thumbnail 2" />
-        <img src="https://placehold.co/100x100" alt="Thumbnail 3" />
+        {displayImages.map((image, index) => (
+          <img
+            key={index}
+            className={styles.gallery_thumbnail}
+            src={image.thumbnail}
+            alt={image.alt}
+            onClick={() => {
+              setDisplayImageSrc(image.src);
+              console.log('image.alt', image.alt)
+            }}
+          />
+        ))}
       </div>
     </div>
   );
