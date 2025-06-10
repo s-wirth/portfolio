0391802f3da4b05ea8bf619180d41908f7e3f019
Sophie Wirth <sophiewirth@macbook-pro.speedport.ip>
2025-06-10 16:43:38 +0200

    src

--- src/app/[painting]/GalleryComponent.tsx
+++ src/app/[painting]/GalleryComponent.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import styles from "./gallery.module.css";
 import { useState } from "react";
+
+const isProd = process.env.NODE_ENV === 'production';
 export default function Gallery({galleryImages} : {galleryImages: any}) {
   const [displayImageIndex, setDisplayImageIndex] = useState(0);
   return (
@@ -31,7 +33,7 @@ export default function Gallery({galleryImages} : {galleryImages: any}) {
 
           }}
         ><div className={styles.arrow_right} /></div>
-        <img
+        <Image
           className={styles.gallery_image}
           height={galleryImages[displayImageIndex] ? galleryImages[displayImageIndex].height : 0}
           width={galleryImages[displayImageIndex] ? galleryImages[displayImageIndex].width : 0}
@@ -43,7 +45,7 @@ export default function Gallery({galleryImages} : {galleryImages: any}) {
         {galleryImages.map((image, index) => (
           <div key={index} 
           className={ index === displayImageIndex ? styles.gallery_thumbnail_selected : styles.gallery_thumbnail} 
-          style={{backgroundImage: `url(${'../' + image.src})`}}
+          style={{backgroundImage: `url(${isProd ? image.src : '../' + image.src})`}}
           onClick={() => {
             setDisplayImageIndex(index);
           }}/>
